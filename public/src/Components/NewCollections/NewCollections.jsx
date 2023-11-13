ab8b24aee44700b1a9425c24ccbf80bbfbe7c067
raji <omoiyaalasso@gmail.com>
2023-11-13 10:18:45 -0800

    new collection section was added with product and it was responsive

---
+++ public/src/Components/NewCollections/NewCollections.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./NewCollections.css";
+import allProduct from "../assets/allproducts.jsx";
+import Items from "../Items/Items";
+
+const random = Math.random();
+
+const NewCollections = () => {
+  return (
+    <div className="new_collections">
+      <h1>NEW COLLECTIONS</h1>
+      <hr />
+      <div className="collections">
+        {allProduct.map((items, i) => {
+          const { title, img, id, new_price, old_price } = items;
+          return (
+            <Items
+              key={i}
+              id={id}
+              img={img}
+              title={title}
+              new_price={new_price}
+              old_price={old_price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default NewCollections;
